96dc4448d28ca53e0b201ba83ae03dce2cb19811
lingeorge88 <lingeorge04@gmail.com>
2024-02-02 21:07:15 -0800

    added auth, user model, configured mongoose

--- server/index.js
+++ server/index.js
@@ -8,11 +8,12 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import {register} from "./controllers/auth.js"
 
 /* CONFIGURATIONS*/ 
+dotenv.config({ path: '/Users/GeorgeBigballer/Desktop/Bootcamp-files/social_media_fullstack/.env' });
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
@@ -32,4 +33,19 @@ const storage = multer.diskStorage({
     cb(null, file.originalname)
    }
 })
-const upload = multer({ storage })
+const upload = multer({ storage });
+
+/* ROUTES WITH FILES*/
+app.post("/auth/register", upload.single("picture"), register);
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 6001;
+mongoose
+.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+})
+.catch((error) => console.log(`${error} did not connect`));
