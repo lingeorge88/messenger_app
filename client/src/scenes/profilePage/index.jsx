7e3912721e15a1f6a9643e14c2e95d7d9f1fc8a7
lingeorge88 <lingeorge04@gmail.com>
2024-02-29 20:40:26 -0800

    set up imports for profilePage

--- client/src/scenes/profilePage/index.jsx
+++ client/src/scenes/profilePage/index.jsx
@@ -1,4 +1,28 @@
+import { Box, useMediaQuery } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import Navbar from "scenes/navbar";
+import FriendListWidget from "scenes/widgets/FriendListWidget";
+import MyPostWidget from "scenes/widgets/MyPostWidget";
+import PostsWidget from "scenes/widgets/PostsWidget";
+import UserWidget from "scenes/widgets/UserWidget";
+
+
 const ProfilePage= () => {
+const [user, setUser] = useState(null);
+const { userId } = useParams();
+const token = useSelector((state) => state.token);
+const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+
+const getUser = async ()=>{
+    const response = await fetch(
+        `http://localhost:3001/users/${userId}`, {
+            method:" GET"
+        }
+    )
+}
+
     return (
         <div>
             ProfilePage
